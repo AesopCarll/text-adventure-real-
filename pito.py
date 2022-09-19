7845dc8daf078d08038bad57bf7bc8d7f7c9f04f
Sopp <chara.eriksen@gmail.com>
2022-09-19 11:35:46 +0200

    added files
    added all my files for text adventure!

---
+++ pito.py
@@ -0,0 +1,20 @@
+from msvcrt import LK_UNLCK
+from re import A
+
+
+print("Jeg ble invitert på å spise middag hos noen jeg ikke kjente, ingen kjente hverandre så vi begynte med å spørre om hverandres navn mens vi ventet på maten ved boret")
+navn=input("Mitt navn er ")
+print("Magen rumlet ")
+print("Når skal vi få maten? sa en ")
+navn=input ("")
+print("shh jeg hører skritt")
+print("vi alle satt stille å var nervøse")
+print("en tv monitor slo seg på, og vi alle skvatt")
+print("dere er alle samlet her for en grunn...")
+navn=input("")
+print("STILLE! sa personen på monitoren")
+print("jeg har samlet dere her for en stor pengegevinst på 20 millioner kroner, MEN! bare den siste som står igjen får denne gevinsten")
+print("nå har dere 2 valg, dra nå for å overleve eller å bli for å kunne vinne")
+valg=input ("A: bli, B: dra ")
+if  valg=="A":
+print("du blir igjen og dere alle ble")
