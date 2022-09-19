f5aa9ff2d93782ef697c1c7286308bd841dcdca7
Sopp <chara.eriksen@gmail.com>
2022-09-19 15:13:19 +0200

    changes
    some changes were made...
    ;))

--- pito.py
+++ pito.py
@@ -15,6 +15,11 @@ navn=input("")
 print("STILLE! sa personen på monitoren")
 print("jeg har samlet dere her for en stor pengegevinst på 20 millioner kroner, MEN! bare den siste som står igjen får denne gevinsten")
 print("nå har dere 2 valg, dra nå for å overleve eller å bli for å kunne vinne")
-valg=input ("A: bli, B: dra ")
+valg=input ("A: bli B: dra ")
 if  valg=="A":
-print("du blir igjen og dere alle ble")
+    print("du blir igjen og dere alle som er igjen, ble drept")
+    print(" :( ")
+if  valg=="B":
+    print("du drar hjem og overlever")
+
+    print("✨")
